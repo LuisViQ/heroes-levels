2d6a381c3a4c6edf7da814dae9762a8bee9c84a0
Luis Queiroz <fileps2009@gmail.com>
2025-12-05 08:37:34 -0300

    feat: atualizações

--- Atividade Formacao TS/Introdução ao React/my-app-ts/src/App.tsx
+++ Atividade Formacao TS/Introdução ao React/my-app-ts/src/App.tsx
@@ -2,15 +2,17 @@ import { Layout } from './components/Layout/Layout';
 import { Box, Flex } from '@chakra-ui/react';
 import { Header } from './components/Header/Header';
 import { LoginCard } from './components/Card/LoginCard';
-
+import { useState } from 'react';
 function App() {
+  const [value, setValue] = useState(0)
+  console.log(useState(0))
   return (
     <Layout>
       <Box
         minH="100vh"
         bgGradient="linear(to-br, purple.600, pink.400)"
       >
-        <Header title="Minha Aplicação Bonita" />
+        <Header title="Minha Aplicação" />
 
         <Flex
           align="center"
